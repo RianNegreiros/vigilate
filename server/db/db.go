7559d5bb4a374d64c2bb71695a19c0c5f9684006
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-10-09 08:02:44 -0300

    chore: setup database connection

---
+++ server/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	db *sql.DB
+}
+
+func NewDatabase() (*Database, error) {
+	db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{db: db}, nil
+}
+
+func (d *Database) Close() error {
+	return d.db.Close()
+}
