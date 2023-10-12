da8b12a1596b0d44b2a9b41dea2c3a53624ca349
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-10-12 10:58:10 -0300

    refactor: create repositories

--- service/repository/postgres/postgres_repository.go
+++ service/repository/postgres/postgres_repository.go
@@ -1,8 +1,10 @@
-package service
+package postgres
 
 import (
 	"context"
 	"database/sql"
+
+	"github.com/RianNegreiros/vigilate/domain"
 )
 
 type DBTX interface {
@@ -16,29 +18,29 @@ type repository struct {
 	db DBTX
 }
 
-func NewRepository(db DBTX) ServiceRepository {
+func NewRepository(db DBTX) domain.ServiceRepository {
 	return &repository{db: db}
 }
 
-func (r *repository) CreateService(ctx context.Context, service *Service) (*Service, error) {
+func (r *repository) CreateService(ctx context.Context, service *domain.Service) (*domain.Service, error) {
 	var lastInsertId int
 	query := "INSERT INTO services(name, description, url, status) VALUES ($1, $2, $3, $4) returning id"
 
 	err := r.db.QueryRowContext(ctx, query, service.Name, service.Description, service.URL, service.Status).Scan(&lastInsertId)
 	if err != nil {
-		return &Service{}, err
+		return &domain.Service{}, err
 	}
 
 	service.ID = int64(lastInsertId)
 	return service, nil
 }
 
-func (r *repository) GetServiceByID(ctx context.Context, id int64) (*Service, error) {
-	s := Service{}
+func (r *repository) GetServiceByID(ctx context.Context, id int64) (*domain.Service, error) {
+	s := domain.Service{}
 	query := "SELECT id, name, description, url, status FROM services WHERE id = $1"
 	err := r.db.QueryRowContext(ctx, query, id).Scan(&s.ID, &s.Name, &s.Description, &s.URL, &s.Status)
 	if err != nil {
-		return &Service{}, err
+		return &domain.Service{}, err
 	}
 
 	return &s, nil
--- user/repository/postgres/postgres_repository.go
+++ user/repository/postgres/postgres_repository.go
@@ -1,8 +1,10 @@
-package user
+package postgres
 
 import (
 	"context"
 	"database/sql"
+
+	"github.com/RianNegreiros/vigilate/domain"
 )
 
 type DBTX interface {
@@ -16,29 +18,29 @@ type repository struct {
 	db DBTX
 }
 
-func NewRepository(db DBTX) Repository {
+func NewRepository(db DBTX) domain.UserRepository {
 	return &repository{db: db}
 }
 
-func (r *repository) CreateUser(ctx context.Context, user *User) (*User, error) {
+func (r *repository) CreateUser(ctx context.Context, user *domain.User) (*domain.User, error) {
 	var lastInsertId int
 	query := "INSERT INTO users(username, password, email) VALUES ($1, $2, $3) returning id"
 
 	err := r.db.QueryRowContext(ctx, query, user.Username, user.Password, user.Email).Scan(&lastInsertId)
 	if err != nil {
-		return &User{}, err
+		return &domain.User{}, err
 	}
 
 	user.ID = int64(lastInsertId)
 	return user, nil
 }
 
-func (r *repository) GetUserByEmail(ctx context.Context, email string) (*User, error) {
-	u := User{}
+func (r *repository) GetUserByEmail(ctx context.Context, email string) (*domain.User, error) {
+	u := domain.User{}
 	query := "SELECT id, email, username, password FROM users WHERE email = $1"
 	err := r.db.QueryRowContext(ctx, query, email).Scan(&u.ID, &u.Email, &u.Username, &u.Password)
 	if err != nil {
-		return &User{}, err
+		return &domain.User{}, err
 	}
 
 	return &u, nil
