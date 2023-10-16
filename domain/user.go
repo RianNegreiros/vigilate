266ee40418ed8519a996fa7f14607c52c56b5b9a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-10-16 14:17:00 -0300

    chore: setup users table

--- domain/user.go
+++ domain/user.go
@@ -6,12 +6,21 @@ import (
 )
 
 type User struct {
-	ID        int64     `json:"id" db:"id"`
-	Username  string    `json:"username" db:"username"`
-	Email     string    `json:"email" db:"email"`
-	Password  []byte    `json:"password" db:"password"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	ID          int64             `json:"id" db:"id"`
+	Username    string            `json:"username" db:"username"`
+	Email       string            `json:"email" db:"email"`
+	Password    []byte            `json:"password" db:"password"`
+	Preferences map[string]string `json:"preferences" db:"preferences"`
+	CreatedAt   time.Time         `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time         `json:"updated_at" db:"updated_at"`
+}
+
+type Preference struct {
+	ID         int64     `json:"id" db:"id"`
+	Name       string    `json:"name" db:"name"`
+	Preference string    `json:"preference" db:"preference"`
+	CreatedAt  time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at" db:"updated_at"`
 }
 
 type CreateUserRequest struct {
--- infra/database/migrations/20231011195229_add_created_at_and_updated_at_columns.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users ADD COLUMN IF NOT EXISTS created_at timestamptz DEFAULT now();
-ALTER TABLE users ADD COLUMN IF NOT EXISTS updated_at timestamptz DEFAULT now();
