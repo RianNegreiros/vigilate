d43057df51595d84cf7902f83f5cc7a738113a59
Rian Negreiros <riannegreiros@gmail.com>
2023-10-28 14:07:38 -0300

    fix: layout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-white text-black dark:bg-gray-900 dark:text-white h-full selection:bg-gray-50 dark:selection:bg-gray-800`}>
+      <body className={`${inter.className} min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white h-full selection:bg-gray-50 dark:selection:bg-gray-800`}>
         <main className='flex flex-col'>
           {children}
         </main>
