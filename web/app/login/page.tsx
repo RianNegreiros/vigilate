9312f90ecfc62ecbbb59171d4394894c3b307ce3
Rian Negreiros <riannegreiros@gmail.com>
2023-10-29 20:09:18 -0300

    refactor: DRY login and register button

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -4,6 +4,7 @@ import { FormEvent, useState } from "react"
 import { LoginData } from "../models"
 import { useRouter } from "next/navigation"
 import { login } from "../util/api"
+import FormButton from "../components/FormButton"
 
 export default function LoginPage() {
   const [loginData, setLoginData] = useState<LoginData>({
@@ -76,9 +77,7 @@ export default function LoginPage() {
                         </div>
                         <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                     </div> */}
-              <button type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
-                in</button>
+              <FormButton text="Sign in" />
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet? <a href="/register"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
--- web/app/register/page.tsx
+++ web/app/register/page.tsx
@@ -4,6 +4,7 @@ import { FormEvent, useState } from "react"
 import { RegisterData } from "../models"
 import { usePathname, useRouter } from "next/navigation"
 import { login, register } from "../util/api"
+import FormButton from "../components/FormButton"
 
 export default function RegisterPage() {
   const [formData, setFormData] = useState<RegisterData>({
@@ -82,7 +83,7 @@ export default function RegisterPage() {
                   <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="terms.html">Terms and Conditions</a></label>
                 </div>
               </div>
-              <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+              <FormButton text="Create account" />
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account? <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
               </p>
