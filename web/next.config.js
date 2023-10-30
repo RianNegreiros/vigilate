a245fece2f36421511b6979097028965f94ed0f6
Rian Negreiros <riannegreiros@gmail.com>
2023-10-30 19:57:42 -0300

    fix: set trusted-origins

--- web/next.config.js
+++ web/next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  Headers: [
-    {
-      key: 'Access-Control-Allow-Origin',
-      value: 'https://vigilate.vercel.app'
-    },
-  ],
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://vigilate.vercel.app',
+          },
+        ],
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
