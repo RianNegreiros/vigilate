91c016902f9d890c1c803be9f3487ae0d3e6f85a
Rian Negreiros <riannegreiros@gmail.com>
2023-10-29 21:00:24 -0300

    refactor: apply DRY to labels and inputs

---
+++ web/app/components/Input.tsx
@@ -0,0 +1,24 @@
+interface AuthInputProps {
+  value: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  name: string
+  id: string
+  placeholder: string
+  type: string
+  required: boolean
+}
+
+export default function Input({ value, onChange, name, id, placeholder, type, required }: AuthInputProps) {
+  return (
+    <input
+      value={value}
+      onChange={onChange}
+      type={type}
+      name={name}
+      id={id}
+      placeholder={placeholder}
+      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      required={required}
+    />
+  )
+}
--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -5,6 +5,10 @@ import { LoginData } from "../models"
 import { useRouter } from "next/navigation"
 import { login } from "../util/api"
 import FormButton from "../components/FormButton"
+import AuthInput from "../components/Input"
+import Label from "../components/Label"
+import AuthHeader from "../components/AuthHeader"
+import Logo from "../components/Logo"
 
 export default function LoginPage() {
   const [loginData, setLoginData] = useState<LoginData>({
@@ -33,38 +37,30 @@ export default function LoginPage() {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <svg className="w-8 h-8 mr-2 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor" viewBox="0 0 20 14">
-            <path
-              d="M10 0C4.612 0 0 5.336 0 7c0 1.742 3.546 7 10 7 6.454 0 10-5.258 10-7 0-1.664-4.612-7-10-7Zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
-          </svg>
+        <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <Logo />
           Vigilate
         </a>
         <div
           className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
-            </h1>
+            <AuthHeader text="Sign in to your account" />
             <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
               <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" name="email" id="email"
-                  value={loginData.email}
+                <Label htmlFor="email" text="Your email" />
+                <AuthInput value={loginData.email}
                   onChange={handleInputChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com" required />
+                  type="email" name="email" id="email" placeholder="johndoe@mail.com"
+                  required
+                />
               </div>
               <div>
-                <label htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input
-                  value={loginData.password}
+                <Label htmlFor="password" text="Your password" />
+                <AuthInput value={loginData.password}
                   onChange={handleInputChange}
-                  type="password" name="password" id="password" placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required />
+                  type="password" name="password" id="password" placeholder="************"
+                  required
+                />
               </div>
               {/* <div className="flex items-center justify-between">
                         <div className="flex items-start">
